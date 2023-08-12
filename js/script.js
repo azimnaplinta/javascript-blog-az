8548304811c74618b7d28a98729ac93ff8058822
Anita Zimna-Plinta <a.zimna-plinta@gmail.com>
2023-08-12 21:00:08 +0200

    Remove class active from all articles and articles links in JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 'use strict';
 
 function titleClickHandler(event){
+  
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+   /*[DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,11 +13,10 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /*[IN PROGRESS] add class 'active' to the clicked link *///
 
 
-
-  /* remove class 'active' from all articles */
+   /*[DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.post.active');
 
@@ -25,6 +25,7 @@ function titleClickHandler(event){
   }
 
   /* get 'href' attribute from the clicked link */
+
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
